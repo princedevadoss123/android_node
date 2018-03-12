776cb712342c678f745c8c2541f596c4336385d9
Prince Devadoss <prince.devadoss@sap.com>
2018-03-13 00:40:46 +0530

    Main Page fixed and aligned.

--- public/js/controller/mainController.js
+++ public/js/controller/mainController.js
@@ -3,11 +3,17 @@ angular.module('main',['customer.services'])
   .controller('MainController',function($scope,UserService) {
     $scope.onInit = function() {
       $scope.text = 'hello';
-    $scope.switchMap = true;
-    $scope.switchTable = false;
-    $scope.switchDelete = false;
-  },
-  $scope.customerOnClick = function(){
-    console.log("hello");
-  }
+      $scope.switchMap = true;
+      $scope.switchTable = false;
+      $scope.switchDelete = false;
+      $(document).ready(function(){
+        $('.nav li span').click(function(event){
+          $('.active').removeClass('active');
+          $(this).addClass('active');
+        });
+      });
+    },
+    $scope.customerOnClick = function(){
+      console.log("hello");
+    }
 });
