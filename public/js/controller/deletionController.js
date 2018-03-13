9bc21c71e1f317a62bdf94d4e65859ef559453dd
unknown <subashri17g@gmail.com>
2018-03-13 20:41:51 +0530

    Trash icon updated

--- public/js/controller/deletionController.js
+++ public/js/controller/deletionController.js
@@ -1,10 +1,15 @@
 // Main Controller
 angular.module('deletion',['customer.services'])
-  .controller('DeletionController',function($scope,UserService) {
+  .controller('DeletionController',function($scope,UserService,$window) {
     $scope.onInit = function() {
       $scope.text = 'hello';
       UserService.getUsers().then(function (response) {
         $scope.entries = response.data;
     });
-    }
+  },
+  $scope.GetDetails = function (index) {
+                var name = $scope.entries[index].name;
+                var email = $scope.entries[index].email;
+                $window.alert("Name: " + name + "\nEmail: " + email);
+            };
 });
